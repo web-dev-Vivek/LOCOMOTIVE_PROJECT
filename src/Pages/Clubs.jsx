798f34f202d577dbb5e19f6f5f3a6e454a59ed82
Vivek Jha <progamervivek2020@gmail.com>
2025-08-07 10:18:40 +0530

    .

--- src/Pages/Clubs.jsx
+++ src/Pages/Clubs.jsx
@@ -2,6 +2,7 @@ import React, { useRef, useEffect } from "react";
 import Navbar from "../component/Navbar";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
+import Club from "../assets/Club.jpg";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -12,36 +13,41 @@ const Clubs = () => {
     gsap.to(ClubRef.current, {
       scrollTrigger: {
         trigger: ClubRef.current,
-        start: "top 40%",
-        end: "top 0%",
-        scrub: 2,
+        start: "top 10%",
+        end: "top -40%",
+        scrub: 3,
         markers: true,
       },
-      y: 300,
+      y: 510,
       ease: "power1.out",
     });
   }, []);
 
   return (
-    <div className="min-h-screen bg-white">
+    <div
+      style={{ backgroundColor: "#f2f7fa" }}
+      className="min-h-screen flex flex-col justify-center items-center "
+    >
       <Navbar />
-      <div className=" w-full h-screen mt-20 px-10 flex items-center justify-start">
-        <h1
-          ref={ClubRef}
-          style={{ fontFamily: "'Playfair Display', serif" }}
-          className="text-6xl md:text-9xl font-bold text-black"
-        >
-          CLUBS
-        </h1>
+      <div className=" w-[95vw] border-b-1  border-gray h-screen mt-20 pb-10 flex items-center justify-between">
+        <div className="w-full h-full flex items-start justify-start ">
+          <h1
+            ref={ClubRef}
+            style={{ fontFamily: "'Playfair Display', serif" }}
+            className="text-6xl md:text-9xl font-bold text-black"
+          >
+            CLUBS
+          </h1>
+        </div>
+        <div className="w-full h-full flex items-end justify-end ">
+          <img
+            className="w-[40vw] flex md:flex-col flex-row"
+            src={Club}
+            alt=""
+          />
+        </div>
       </div>
-      <div className=" w-full h-screen mt-20 px-10 flex items-center justify-start">
-        <h1
-          style={{ fontFamily: "'Playfair Display', serif" }}
-          className="text-6xl md:text-9xl font-bold text-black"
-        >
-          CLUBS
-        </h1>
-      </div>
+      <div className="w-screen h-screen "></div>
     </div>
   );
 };
