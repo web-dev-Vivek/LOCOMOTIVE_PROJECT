2300c9eaacf2a9dc38d01df4f5c51b030bad5cb8
Vivek Jha <progamervivek2020@gmail.com>
2025-09-20 15:37:26 +0530

    remove Introscreen page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 // src/App.js
-import React, { useState } from "react";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 // Pages
-import IntroScreen from "./Pages/IntroScreen";
-import Home from "./Pages/Home"; // Home page
+import Home from "./Pages/Home";
 import Team from "./Pages/Team";
 import Announcement from "./Pages/Announcement";
 import Clubs from "./Pages/Clubs";
@@ -12,22 +11,16 @@ import Cells from "./Pages/Cells";
 import Events from "./Pages/Events";
 
 function App() {
-  const [showIntro, setShowIntro] = useState(true);
-
   return (
     <div className="relative">
-      {showIntro ? (
-        <IntroScreen onAnimationComplete={() => setShowIntro(false)} />
-      ) : (
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/events" element={<Events />} />
-          <Route path="/team" element={<Team />} />
-          <Route path="/announcement" element={<Announcement />} />
-          <Route path="/clubs" element={<Clubs />} />
-          <Route path="/cells" element={<Cells />} />
-        </Routes>
-      )}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/announcement" element={<Announcement />} />
+        <Route path="/clubs" element={<Clubs />} />
+        <Route path="/cells" element={<Cells />} />
+      </Routes>
     </div>
   );
 }
